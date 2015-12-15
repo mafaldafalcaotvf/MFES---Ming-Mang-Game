69adefb7e7333b45b612ee77523169229b3f4bf4
JoaoSoares <joaosoaresacs@gmail.com>
2015-12-15 05:39:50 +0000

    win condition

--- generated/java/src/MingMang/MingMang.java
+++ generated/java/src/MingMang/MingMang.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 @SuppressWarnings("all")
 public class MingMang {
-    private Board board = new Board();
+    public Board board = new Board();
     public Object currentColor = MingMang.quotes.WhiteQuote.getInstance();
 
     public MingMang() {
@@ -54,6 +54,71 @@ public class MingMang {
                 return false;
             }
         }
+    }
+
+    public Boolean gameOver() {
+        Number i = 1L;
+        Number j = 1L;
+        Object cmp = MingMang.quotes.BlankSpaceQuote.getInstance();
+        Boolean whileCond_7 = true;
+
+        while (whileCond_7) {
+            whileCond_7 = !(Utils.equals(i, 8L));
+
+            if (!(whileCond_7)) {
+                break;
+            }
+
+            {
+                Boolean whileCond_8 = true;
+
+                while (whileCond_8) {
+                    whileCond_8 = !(Utils.equals(j, 8L));
+
+                    if (!(whileCond_8)) {
+                        break;
+                    }
+
+                    if (Utils.equals(cmp,
+                                MingMang.quotes.BlankSpaceQuote.getInstance())) {
+                        if (Utils.equals(board.getPiece(SeqUtil.seq(i, j)),
+                                    MingMang.quotes.BlankSpaceQuote.getInstance())) {
+                            /* skip */
+                        } else {
+                            if (Utils.equals(board.getPiece(SeqUtil.seq(i, j)),
+                                        MingMang.quotes.WhiteQuote.getInstance())) {
+                                cmp = MingMang.quotes.WhiteQuote.getInstance();
+                            } else {
+                                if (Utils.equals(board.getPiece(SeqUtil.seq(i, j)),
+                                            MingMang.quotes.BlackQuote.getInstance())) {
+                                    cmp = MingMang.quotes.BlackQuote.getInstance();
+                                }
+                            }
+                        }
+                    } else {
+                        Boolean andResult_9 = false;
+
+                        if (!(Utils.equals(board.getPiece(SeqUtil.seq(i, j)),
+                                    MingMang.quotes.BlankSpaceQuote.getInstance()))) {
+                            if (!(Utils.equals(board.getPiece(SeqUtil.seq(i, j)),
+                                        cmp))) {
+                                andResult_9 = true;
+                            }
+                        }
+
+                        if (andResult_9) {
+                            return false;
+                        }
+                    }
+
+                    j = j.longValue() + 1L;
+                }
+
+                i = i.longValue() + 1L;
+            }
+        }
+
+        return true;
     }
 
     public void changePlayer() {
